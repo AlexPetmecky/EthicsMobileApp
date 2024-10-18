29caba9ed4e6a11291bdabef5788aad840288100
alexpetmecky <petmeckyalex@gmail.com>
2024-10-18 13:10:42 -0500

    Initial Commit

---
+++ MobileAppEthics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileAppEthics
+//
+//  Created by Alex Petmecky on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
