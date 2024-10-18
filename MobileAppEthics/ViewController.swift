18f4896ce8f62b74d22422a981fee2e5485e966f
themileschen <mileschen@smu.edu>
2024-10-18 13:48:32 -0500

    test

--- MobileAppEthics/ViewController.swift
+++ MobileAppEthics/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     //EDITS
+    // test (miles)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
