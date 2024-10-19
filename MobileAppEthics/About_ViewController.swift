e41a6e237737afb2ff8402845f12064a1bc07d4b
themileschen <mileschen@smu.edu>
2024-10-19 17:20:39 -0500

    redo complete

---
+++ MobileAppEthics/About_ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  About_ViewController.swift
+//  MobileAppEthics
+//
+//  Created by Miles Chen on 10/19/24.
+//
+
+import UIKit
+
+class About_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green // for testing
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
