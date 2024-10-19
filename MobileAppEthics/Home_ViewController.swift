39a35197f6b4bddcc347dd7e3b315c963a660fcf
themileschen <mileschen@smu.edu>
2024-10-19 16:57:23 -0500

    redo 3

--- MobileAppEthics/Home_ViewController.swift
+++ MobileAppEthics/Home_ViewController.swift
@@ -13,6 +13,8 @@ class Home_ViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red // for testing
+
     }
     
 
